70adb36549237af76fab3c02c1f764781d0736ee
Atlas Kaisar <atlas.kaisar@icloud.com>
2025-02-11 19:16:55 +0100

    fix: update source_files paths in podspec

--- package/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['@react-native', 'plugin:prettier/recommended'],
-  plugins: ['prettier'],
-  ignorePatterns: ['scripts', 'lib'],
-  rules: {
-    'prettier/prettier': ['warn'],
-  },
-};
